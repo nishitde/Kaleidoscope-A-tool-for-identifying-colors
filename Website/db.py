081de065da0bb3539adb5d19f4ab8040d2a176a6
nishitde <nishitde97@gmail.com>
2020-06-08 00:28:41 -0400

    Final changes

--- Website/db.py
+++ Website/db.py
@@ -4,7 +4,7 @@ import sqlite3
 import cv2
 
 SQLITE_PATH = os.path.join(os.path.dirname(__file__), 'kaleidoscope.db')
-DIRECTORY = 'D:\Drexel Work\Spring-20\Computer Vision\Project\Kaleidoscope-A-tool-for-identifying-colors\Website\public\img'
+DIRECTORY = 'D:\Drexel Work\Spring-20\Computer Vision\Project\Kaleidoscope2\Website\public\img'
 
 image_names = []
 
@@ -24,42 +24,63 @@ class Database:
         self.conn.commit()
 
     def images_list(self):
+        """
+            Returns images from the folder.
+        """
         img = self.select('SELECT * FROM Images')
         return img
 
     def load_images(self):
+        """
+            If file does not exist, the entry is made in the database, else error is returned.
+        """
         for file in os.listdir(DIRECTORY):
-            image_names.append(file)
+            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
+                image_names.append(file)
         try:
-            for i in image_names[1:]:
+            for i in image_names:
                 self.execute('INSERT INTO Images (Name) VALUES (?)', [i])
             return "Good"
         except sqlite3.Error as er:
             return er
 
     def create_images(self, name):
+        """
+            Validates for duplicate file in the folder.
+        """
         try:
             self.execute('INSERT INTO Images (Name) VALUES (?)', [name])
             return "Good"
         except sqlite3.Error as er:
             return er
+    def get_image(self, image_path):
+        """
+            Returns RGB of the images
+        """
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
 
     def get_image_matrix(self):
+        """
+            Returns image matrix of the images in the directory.
+        """
         images = []
         for file in os.listdir(DIRECTORY):
             if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
                 images.append(self.get_image(os.path.join(DIRECTORY, file)))
         return images
 
-    def get_image(self, image_path):
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image
-
     def create_color(self, name):
+        """
+            Stores the color selected by the user in the database.
+        """
         self.execute('INSERT INTO colors (Name) VALUES (?)',[name])
 
     def delete_color(self):
+        """
+            Deletes the color stored in the database.
+        """
         self.execute('DELETE FROM colors')
 
     def close(self):
--- Website/run.py
+++ Website/run.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, send_file, g, request, jsonify, session, escape, redirect
-from passlib.hash import pbkdf2_sha256
 import os
-import sqlite3
 from db import Database
 from sklearn.cluster import KMeans
 import numpy as np
@@ -11,7 +9,6 @@ from skimage.color import rgb2lab, deltaE_cie76
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder='public', static_url_path='')
-app.secret_key = b'lkj98t&%$3rhfSwu3D'
 DIRECTORY = 'D:\Drexel Work\Spring-20\Computer Vision\Project\Kaleidoscope-A-tool-for-identifying-colors\Website\public\img'
 app.config['DIRECTORY'] = DIRECTORY
 
@@ -29,13 +26,35 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-# Handle the home page.
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/uploadImage')
+def uploadImage():
+    """ Upload screen """
+    return render_template('uploadImage.html')
+
+
+@app.route('/file_saved')
+def save():
+    """ Uploaded file saved. """
+    return render_template('file_saved.html')
+
+
+@app.route('/file_unsaved')
+def unsave():
+    """ Uploaded file not saved. """
+    return render_template('file_unsaved.html')
+
+@app.route('/aboutus')
+def aboutus():
+    """ About Us screen """
+    return render_template('aboutus.html')
+
 # Handle any files that begin "/" by loading from the packages directory.
 @app.route('/packages/<path:path>')
+
 def base_static(path):
     return send_file(os.path.join(app.root_path, '..', 'packages', path))
 
@@ -47,80 +66,121 @@ def api_imageList():
 # Handles the insertion of images in the Database.
 @app.route('/pictures')
 def pictures():
+    """ Renders all the images from the folder. """
     a = get_db().load_images()
     if a == "Good":
         return render_template('pictures.html')
     else:
         return render_template('pictures.html')
 
-# Handles the task of uploading the file onto a physical memory.
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['inpFile']
-      if f:
-          filename = secure_filename(f.filename)
-          print("filename '" +filename+ "' is uploaded.")
-          f.save(os.path.join(app.config['DIRECTORY'], filename))
-      return render_template('file_saved.html')
+    """
+    Returns a  template that indicates that the file has been saved successfully or returns a template that indicates the file already exists.
+    """
+    if request.method == 'POST':
+        f = request.files['inpFile']
+        if f:
+            filename = secure_filename(f.filename)
+            val = get_db().create_images(filename)
+            if val == "Good":
+                f.save(os.path.join(app.config['DIRECTORY'], filename))
+                return render_template('file_saved.html')
+            else:
+                return render_template('file_unsaved.html')
 
-# Function to flatten the array to pass as input, and to arrange the colors in the right order.
-def get_colors(image, clusters):
-    new_image = cv2.resize(image, (600, 400), interpolation=cv2.INTER_AREA)
-    shape = new_image.shape[0] * new_image.shape[1]
-    new_image = new_image.reshape(shape, 3)
 
-    z = KMeans(n_clusters=clusters)
-    labels = z.fit_predict(new_image)
+def predominant_colors(image, number_of_colors):
+    """
+            Function uses rgb2lab to convert the RGB color space into LAB color space, and compute the similarities using cie.
+            Args:
+                images:  All the images in the folder in terms of matrices.
+                number_of_colors: Number of predominant colors to be chosen.
+            Returns:
+                Predominant colors from the images.
 
-    counts = Counter(labels)
-    counts = dict(sorted(counts.items()))
+    """
+    resized_img = cv2.resize(image, (600, 400), interpolation=cv2.INTER_AREA)
+    d_1, d_2 = resized_img.shape[0], resized_img.shape[1]
+    modified_image = resized_img.reshape(d_1 * d_2, 3)
 
-    center_colors = z.cluster_centers_
+    kmeans_output = KMeans(n_clusters=number_of_colors)
+    var_labels = kmeans_output.fit_predict(modified_image)
 
-    color_dist = [center_colors[i] for i in counts.keys()]
-    rgb = [color_dist[i] for i in counts.keys()]
+    # To store the counts such that the maximum frequency colors are selected as predominant colors.
+    counts = Counter(var_labels)
+    counts_freq = dict(sorted(counts.items()))
+
+    center_colors = kmeans_output.cluster_centers_
+
+    ordered_list = [center_colors[i] for i in counts_freq.keys()]
+    rgb = [ordered_list[i] for i in counts_freq.keys()]
 
     return rgb
 
-# Function to try and match to the top 10 color of the images. We use rgb2lab to convert the values, and compute differences using cie colorspace.
-def resultant_image(image, color, threshold=60, num_colors=10):
-    image_colors = get_colors(image, num_colors)
-    z = np.asarray([[color]])
-    z1 = np.uint8(z)
-    selected_color = rgb2lab(z1)
+def matched_image(image, color, threshold=60, number_of_colors=10):
+    """
+        Function uses rgb2lab to convert the RGB color space into LAB color space, and compute the similarities using cie.
+        Args:
+            image:  Every image in the folder.
+            color: Chosen color.
+            threshold: A value below which the color of the images are considered to be matching with the chosen color.
+            number_of_colors: Number of predominant colors to be chosen.
 
-    select_image = False
-    for i in range(num_colors):
-        y = np.asarray([[image_colors[i]]])
-        y1 = np.uint8(y)
-        current_color = rgb2lab(y1)
-        difference = deltaE_cie76(selected_color, current_color)
-        if (difference < threshold):
-            select_image = True
+        Returns:
+            An image matrix of the images with difference (between chosen color and the color in the images) less than the threshold value.
 
-    return select_image
+    """
+    selected = False
+    image_colors = predominant_colors(image, number_of_colors)
+    lab_color = rgb2lab(np.uint8(np.asarray([[color]])))
 
-# Function to go through all images in the set, and return relevant images as output.
-def get_images(images, image_names, color, threshold, colors_to_match):
-    index = 1
+    for i in range(number_of_colors):
+        input_color = rgb2lab(np.uint8(np.asarray([[image_colors[i]]])))
+        difference_val = deltaE_cie76(lab_color, input_color)
+        if (difference_val < threshold):
+            selected = True
 
+    return selected
+
+def selected_images(images, image_names, color, threshold, colors_to_match):
+    """
+        Function uses rgb2lab to convert the RGB color space into LAB color space, and compute the similarities using cie.
+        Args:
+            images:  All the images in the folder in terms of matrices.
+            image_names:  The names of the images in the folder.
+            color: Color chosen by the users.
+            threshold: A value below which the color of the images are considered to be matching with the chosen color.
+
+        Returns:
+            An image matrix of the images with difference (between chosen color and the color in the images) less than the threshold value.
+
+    """
     selected_images = []
+    color_index = 1
 
     for i in range(len(images)):
-        selected = resultant_image(images[i], color, threshold, colors_to_match)
+        selected = matched_image(images[i], color, threshold, colors_to_match)
         if (selected):
             selected_images.append(image_names[i])
-            index += 1
+            color_index += 1
     print("Selected Images:", selected_images)
 
     return selected_images
 
-# Handles the storing of colors in the Database.
 @app.route('/store_color', methods=['GET', 'POST'])
 def store_color():
+    """
+        Function stores the selected color in the database and returns the images of the same color.
+        Args: NA
+
+        Returns:
+            An image matrix of the images with difference (between chosen color and the color in the images) less than the threshold value.
+
+    """
     name = ""
-    result = []
+    final_matched_images = []
     if request.method == 'POST':
         print("\nComputing the images to be displayed based on the selected color...")
         image_names = []
@@ -129,12 +189,13 @@ def store_color():
         get_db().create_color(name)
         images = get_db().get_image_matrix()
         for file in os.listdir(DIRECTORY):
-            print("Finished scanning '" +file+ "'")
-            image_names.append(file)
-        result = get_images(images, image_names, COLORS[name], 60, 5)
+            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
+                print("Finished scanning '" +file+ "'")
+                image_names.append(file)
+        final_matched_images = selected_images(images, image_names, COLORS[name], 60, 5)
         get_db().delete_color()
 
-    return render_template('selection_pictures.html', name = name, hey = result)
+    return render_template('selection_pictures.html', name = name, final_matched_images = final_matched_images)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
